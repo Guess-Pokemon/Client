faf5579ea87f4dd2d00a4a5691b45e57a28c2d7e
ForestoAY <forestoay@gmail.com>
2024-09-11 20:11:14 +0700

    feat: add react context on DarkModeToggle

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
+import { DarkModeProvider } from "./context/DarkModeContext";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <DarkModeProvider>
+        <RouterProvider router={router} />;
+      </DarkModeProvider>
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/context/DarkModeContext.jsx
@@ -0,0 +1,23 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+const DarkModeContext = createContext();
+
+export const DarkModeProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(true);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [darkMode]);
+
+  return (
+    <DarkModeContext.Provider value={{ darkMode, setDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+};
+
+export const useDarkMode = () => useContext(DarkModeContext);
