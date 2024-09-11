a4add274e5475c42980aa76ce8aa9710706c710e
ForestoAY <forestoay@gmail.com>
2024-09-11 10:58:18 +0700

    feat: add navbar and rootlayout, config route

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -1,9 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
 import PokemonGuessingGame from "./PokemonGuessingGame";
+import RootLayout from "./layouts/RootLayout";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <PokemonGuessingGame />,
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/login",
+        element: <h1>login page</h1>,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        index: true,
+        element: <PokemonGuessingGame />,
+      },
+    ],
   },
 ]);
