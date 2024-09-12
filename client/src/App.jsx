3692b60695741d7f9d247cca30b674844c5dc8d4
ForestoAY <forestoay@gmail.com>
2024-09-12 08:09:09 +0700

    refactor: change DarkModeProvider from App to RootLayout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,8 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
-import { DarkModeProvider } from "./context/DarkModeContext";
 
 function App() {
-  return (
-    <>
-      <DarkModeProvider>
-        <RouterProvider router={router} />;
-      </DarkModeProvider>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
